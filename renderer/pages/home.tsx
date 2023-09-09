e9e54aa4136363f853f0c511f1b3cecf1397f2e5
Amine derbal <aminedoran31@gmail.com>
2023-09-09 17:33:28 +0100

    Update Home page to provide the store

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import { Provider } from 'react-redux';
+import Url from '../components/Url';
+import store from '../redux/store';
+import Novel from '../components/Novel';
 
-function Home() {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-typescript-tailwindcss)</title>
-      </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-red-900 text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
-        <span className='text-Cyan'>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/next'>
-          <a className='btn-blue'>Go to next page</a>
-        </Link>
-      </div>
-    </React.Fragment>
-  );
-}
+const Home = () => (
+  <Provider store={store}>
+    <h1 className='text-center mt-5'>Welcome</h1>
+    <Url />
+    <Novel />
+  </Provider>
+);
 
 export default Home;
