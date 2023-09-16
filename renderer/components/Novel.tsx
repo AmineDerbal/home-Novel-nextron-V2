42e0678316c42142644601252d2c4736733ee86c
Amine derbal <aminedoran31@gmail.com>
2023-09-16 18:06:29 +0100

    Display Errors when fetching novel data

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -3,21 +3,21 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store'; // Make sure to import the correct RootState type
 
 const Novel = () => {
-  const { novelData, isLoading, hasError } = useSelector(
+  const { novelData, isLoading, hasError,error } = useSelector(
     (state: RootState) => state.novel
   );
 
   if (isLoading) {
-    return <div className="text-red-100">Loading...</div>;
+    return <div>Loading...</div>;
   }
-  if (hasError ) {
-    return <div>Error...</div>;
+  if (hasError) {
+    return <div className='text-white'>Error... {error}</div>;
   }
   if (!novelData || Object.keys(novelData).length === 0) {
     return <div>Waiting for url...</div>;
   }
-  
-  return <div className="mt-5 text-red-100">{novelData.serieName}</div>;
+
+  return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
 };
 
 export default Novel;
