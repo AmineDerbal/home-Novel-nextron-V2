7c01ff8d3be95835126dbbd204aba17a36aed75d
Amine derbal <aminedoran31@gmail.com>
2023-09-17 18:11:09 +0100

    Format using tsx standard

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -3,21 +3,19 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store'; // Make sure to import the correct RootState type
 
 const Novel = () => {
-  const { novelData, isLoading, hasError,error } = useSelector(
-    (state: RootState) => state.novel
-  );
+	const { novelData, isLoading, hasError, error } = useSelector((state: RootState) => state.novel);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (hasError) {
-    return <div className='text-white'>Error... {error}</div>;
-  }
-  if (!novelData || Object.keys(novelData).length === 0) {
-    return <div>Waiting for url...</div>;
-  }
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+	if (hasError) {
+		return <div className="text-white">Error... {error}</div>;
+	}
+	if (!novelData || Object.keys(novelData).length === 0) {
+		return <div>Waiting for url...</div>;
+	}
 
-  return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
+	return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
 };
 
 export default Novel;
