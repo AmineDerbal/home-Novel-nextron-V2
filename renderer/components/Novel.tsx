7bd6a325b426cdd29e4b51d3b16b038dcf5eaa6e
Amine derbal <aminedoran31@gmail.com>
2023-09-28 18:03:38 +0100

    Implement Loader

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store'; // Make sure to import the correct RootState type
+import Loader from './Loader';
 
 const Novel = () => {
-	const { novelData, isLoading, hasError, error } = useSelector((state: RootState) => state.novel);
+  const { novelData, isLoading, hasError, error } = useSelector(
+    (state: RootState) => state.novel,
+  );
 
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
-	if (hasError) {
-		return <div className="text-white">Error... {error}</div>;
-	}
-	if (!novelData || Object.keys(novelData).length === 0) {
-		return <div>Waiting for url...</div>;
-	}
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (hasError) {
+    return <div className="text-white">Error... {error}</div>;
+  }
+  if (!novelData || Object.keys(novelData).length === 0) {
+    return <div>Waiting for url...</div>;
+  }
 
-	return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
+  return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
 };
 
 export default Novel;
