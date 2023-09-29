7c23c9d35eb04e1651b4e47a0e4e49c73a2718b7
Amine derbal <aminedoran31@gmail.com>
2023-09-29 17:16:53 +0100

    Fix typo to detect when novelData is empty

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -8,17 +8,39 @@ const Novel = () => {
     (state: RootState) => state.novel,
   );
 
+  const isNovelEmpty = (obj: any) => {
+    if (
+      obj['authorLink'] !== '' ||
+      obj['authorName'] !== '' ||
+      obj['chapters'].length > 0 ||
+      obj['lastUpdate'] !== '' ||
+      obj['serieImageSrc'] !== '' ||
+      obj['serieName'] !== '' ||
+      obj['synopsis'] !== ''
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  console.log('novelData', isNovelEmpty(novelData));
+
   if (isLoading) {
     return <Loader />;
   }
   if (hasError) {
     return <div className="text-white">Error... {error}</div>;
   }
-  if (!novelData || Object.keys(novelData).length === 0) {
+  if (!novelData || isNovelEmpty(novelData)) {
     return <div>Waiting for url...</div>;
   }
 
-  return <div className="mt-5"> {hasError ? 'true' : 'false'}</div>;
+  return (
+    <div className="mt-5 text-center text-white">
+      {' '}
+      Hello {novelData.authorName}
+    </div>
+  );
 };
 
 export default Novel;
