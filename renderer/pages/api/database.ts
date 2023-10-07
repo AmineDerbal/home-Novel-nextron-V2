5dc6323329b4b645017f741e58a5f350ef7c1480
Amine derbal <aminedoran31@gmail.com>
2023-10-07 18:23:15 +0100

    Request to DB

---
+++ renderer/pages/api/database.ts
@@ -0,0 +1,19 @@
+import db from '../../utils/db';
+
+const handler = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      await db();
+      res.json({ message: 'connected' });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: 'An error occurred while fetching the data' });
+    }
+  } else {
+    res.status(400).json({ error: 'Invalid request method' });
+  }
+  return 'No post';
+};
+
+export default handler;
