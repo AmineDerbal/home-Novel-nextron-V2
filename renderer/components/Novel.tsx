a2b93a0ff7c6d61855601344c9e117a2df48dffe
Amine derbal <aminedoran31@gmail.com>
2023-10-15 16:30:14 +0100

    Update the display

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -80,7 +80,7 @@ const Novel = () => {
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
             </svg>
           </div>
-          <h2 className="text-4xl">
+          <h2 className="text-5xl">
             <a
               onClick={(event) => {
                 event.preventDefault();
@@ -92,12 +92,12 @@ const Novel = () => {
               {novelData.serieName}
             </a>
           </h2>
-          <p className="mt-2 text-xl">
-            By <a href={novelData.authorLink}>{novelData.authorName}</a>
+          <p className="mt-2 text-gray-400 text-2xl">
+            <a href={novelData.authorLink}>{novelData.authorName}</a>
           </p>
-          <p>Last Update: {novelData.lastUpdate}</p>
-          <p>{novelData.chapters.length} Chapter(s) </p>
-          <p>Synopsis: {novelData.synopsis}</p>
+          <p className="mt-2 text-gray-400 text-2xl">
+            Last Update: {novelData.lastUpdate}
+          </p>
         </div>
       </div>
       <button
