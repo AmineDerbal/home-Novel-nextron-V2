5d11ffa21972bebfac803db5b66abac658709007
Amine derbal <aminedoran31@gmail.com>
2023-10-16 18:51:09 +0100

    Update button text

--- renderer/components/Url.tsx
+++ renderer/components/Url.tsx
@@ -35,7 +35,7 @@ const Url = () => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleClick}
       >
-        Download
+        CHECK
       </button>
     </div>
   );
