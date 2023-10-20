5a483c32a56702a52dc6ecd7231831ff0c345be5
Amine derbal <aminedoran31@gmail.com>
2023-10-20 15:18:37 +0100

    Add method that update the download path

--- renderer/utils/config.ts
+++ renderer/utils/config.ts
@@ -2,10 +2,24 @@ import path from 'path';
 import fs from 'fs';
 
 const getConfigJsonPath = () => path.join(process.cwd(), 'config.json');
-const getDefaultDownloadPath = async (configJsonPath: string) => {
+
+const getDownloadData = async () => {
+  const configJsonPath = getConfigJsonPath();
   try {
     const data = await fs.promises.readFile(configJsonPath);
     const jsonData = JSON.parse(data.toString());
+    return jsonData;
+  } catch (err) {
+    console.error('Error getting download data:', err);
+    return null;
+  }
+};
+const getDefaultDownloadPath = async () => {
+  try {
+    const jsonData = await getDownloadData();
+    if (!jsonData) {
+      return { success: false };
+    }
     const defaultDownloadPath = jsonData.defaultDownloadPath;
     return {
       success: true,
@@ -17,4 +31,20 @@ const getDefaultDownloadPath = async (configJsonPath: string) => {
   }
 };
 
-export { getConfigJsonPath, getDefaultDownloadPath };
+const updateDefaultDownloadPath = async (defaultDownloadPath: string) => {
+  const configJsonPath = getConfigJsonPath();
+  try {
+    const jsonData = await getDownloadData();
+    if (!jsonData) {
+      return false;
+    }
+    jsonData.defaultDownloadPath = defaultDownloadPath;
+    await fs.promises.writeFile(configJsonPath, JSON.stringify(jsonData));
+    return true;
+  } catch (err) {
+    console.error('Error updating default download path:', err);
+    return false;
+  }
+};
+
+export { getDefaultDownloadPath, updateDefaultDownloadPath };
