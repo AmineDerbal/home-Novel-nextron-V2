84f6721839c505968c4fb85acddfe5e7987a9700
Amine derbal <aminedoran31@gmail.com>
2023-10-23 16:59:52 +0100

    Fix bug when download modal is triggered

--- renderer/components/Novel.tsx
+++ renderer/components/Novel.tsx
@@ -39,6 +39,10 @@ const Novel = () => {
     }
   };
 
+  const startDownload = async () => {
+    await downlodaNovel(novelData);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       if (!isNovelEmpty(novelData)) {
@@ -122,9 +126,7 @@ const Novel = () => {
               <button
                 className=" w-[110px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={() => {
-                  async () => {
-                    await downlodaNovel(novelData);
-                  };
+                  startDownload();
                 }}
               >
                 Download
