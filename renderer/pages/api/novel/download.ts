a5b2b72098a5c634abea2276c4542ebe6a550a50
Amine derbal <aminedoran31@gmail.com>
2023-10-23 14:36:30 +0100

    Setup the download novel API

---
+++ renderer/pages/api/novel/download.ts
@@ -0,0 +1,17 @@
+import buildDownload from '../../../services/buildDownload';
+
+const handler = async (req: any, res: any) => {
+  if (req.method === 'POST') {
+    const { novel } = req.body;
+    try {
+      const response = await buildDownload(novel);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: 'An error occurred while fetching the data' });
+    }
+  }
+  return res.status(400).json({ error: 'Invalid request method' });
+};
+
+export default handler;
