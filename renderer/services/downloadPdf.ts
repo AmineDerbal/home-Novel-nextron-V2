9d1e025b046bd0ce93c67d034cb1bcd4f24ca2e0
Amine derbal <aminedoran31@gmail.com>
2023-10-24 13:02:55 +0100

    Add Novel Image to the PDf downloaded

--- renderer/services/downloadPdf.ts
+++ renderer/services/downloadPdf.ts
@@ -1,5 +1,6 @@
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
+import axios from 'axios';
 import { getDefaultDownloadPath } from '../utils/config';
 
 const pipePdf = async (doc: PDFDocument, serieName: string) => {
@@ -12,7 +13,25 @@ const pipePdf = async (doc: PDFDocument, serieName: string) => {
   );
   doc.font('Times-Bold').fontSize(35).text(serieName, { align: 'center' });
   doc.moveDown();
-  return doc;
+};
+
+const fetchImage = async (src: string) => {
+  const image = await axios.get(src, {
+    responseType: 'arraybuffer',
+  });
+  return image.data;
+};
+
+const generateSerieImage = async (doc: PDFDocument, serieImageSrc: string) => {
+  const image = await fetchImage(serieImageSrc);
+  const imageWidth = 180;
+
+  doc
+    .image(image, doc.page.width / 2 - imageWidth / 2, doc.y, {
+      width: imageWidth,
+    })
+    .stroke();
+  doc.moveDown(0.5);
 };
 
 const createPdf = () => {
@@ -21,4 +40,4 @@ const createPdf = () => {
   return doc;
 };
 
-export { createPdf, pipePdf };
+export { createPdf, pipePdf, generateSerieImage };
