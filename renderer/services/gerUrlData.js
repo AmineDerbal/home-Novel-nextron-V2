6a72c868b4879414be0bbad5ebf761f335f6f1cf
Amine derbal <aminedoran31@gmail.com>
2023-10-26 18:09:12 +0100

    Refactor the Scrappin process

--- renderer/services/gerUrlData.js
+++ renderer/services/gerUrlData.js
@@ -1,8 +1,4 @@
-import puppeteer from 'puppeteer-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import { executablePath } from 'puppeteer';
-
-puppeteer.use(StealthPlugin());
+import { openBrowser, openPage } from './executePuppeteer';
 
 const isNextPageLink = async (page) => {
   try {
@@ -67,23 +63,16 @@ const generateChaptersList = async (page, chaptersList = []) => {
 };
 
 const getData = async (url) => {
-  const browser = await puppeteer.launch({
-    executablePath: executablePath(),
-    headless: false,
-  });
   try {
-    const page = await browser.newPage();
-
-    await page.setExtraHTTPHeaders({
-      'user-agent':
-        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-      'upgrade-insecure-requests': '1',
-      accept:
-        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-    });
-    await page.setViewport({ width: 1280, height: 720 });
+    const browser = await openBrowser();
+    if (!browser) {
+      throw new Error('an error has occured');
+    }
+    const newPage = await openPage(browser);
+    if (!newPage.success) {
+      throw new Error('an error has occured');
+    }
+    const { page } = newPage;
     const response = await page.goto(url, {
       waitUntil: 'domcontentloaded',
       timeout: 0,
