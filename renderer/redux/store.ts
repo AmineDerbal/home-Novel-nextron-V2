1ebd1d97aa18410a9df2fd43ff43b9456e6d6e1c
Amine derbal <aminedoran31@gmail.com>
2023-10-29 13:59:08 +0100

    import modal reducer

--- renderer/redux/store.ts
+++ renderer/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import novelReducer from './novel/novelSlice';
+import modalReducer from './modal/modalSlice';
 
 const store = configureStore({
   reducer: {
     novel: novelReducer,
+    modal: modalReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>; // Define and export RootState type
