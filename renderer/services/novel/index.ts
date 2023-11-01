8f20e5b6402c5acb87b8994632c849849f346ef7
Amine derbal <aminedoran31@gmail.com>
2023-11-01 16:40:26 +0100

    Fix imports and exports

--- renderer/services/novel/index.ts
+++ renderer/services/novel/index.ts
@@ -1,5 +1,6 @@
 import checkNovel from './check';
 import deleteNovel from './delete';
 import createNovel from './create';
+import { updateProgress, getProgress } from './progress';
 
-export { checkNovel, deleteNovel, createNovel };
+export { checkNovel, deleteNovel, createNovel, updateProgress, getProgress };
