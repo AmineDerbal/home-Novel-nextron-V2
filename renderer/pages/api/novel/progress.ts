7fef8c5f31ae56fbf1a97de1e695be0f128f8c5a
Amine derbal <aminedoran31@gmail.com>
2023-11-01 16:36:29 +0100

    Fix progress API bugs

--- renderer/pages/api/novel/progress.ts
+++ renderer/pages/api/novel/progress.ts
@@ -5,16 +5,24 @@ const handler = async (req, res) => {
     currentChapter: 0,
     progress: 0,
   };
-
   if (req.method === 'POST') {
-    const { progress } = req.body;
-    currentProgress = progress;
-    return res.json({ success: 'success' });
-  }
-  if (req.method === 'GET') {
+    console.log('Start of progress POST request');
+    console.log(currentProgress);
+    try {
+      const progress = req.body;
+      console.log('progress', progress);
+      currentProgress = progress;
+      // console.log('Received progress:', currentProgress);
+      return res.json({ success: 'success' });
+    } catch (error) {
+      console.error('Error handling POST request:', error);
+      return res.status(500).json({ error: 'Server error' });
+    }
+  } else if (req.method === 'GET') {
     return res.json({ success: true, currentProgress });
+  } else {
+    res.status(400).json({ error: 'Invalid request method' });
   }
-  return res.status(400).json({ error: 'Invalid request method' });
 };
 
 export default handler;
