ff479ca494eeb740798060066a69ebdd60bf634a
Amine derbal <aminedoran31@gmail.com>
2023-11-02 15:46:54 +0100

    Add method to get & update progress in json config

--- renderer/utils/config.ts
+++ renderer/utils/config.ts
@@ -31,6 +31,27 @@ const getDefaultDownloadPath = async () => {
   }
 };
 
+const getDownloadProgress = async () => {
+  const configJsonPath = getConfigJsonPath();
+  try {
+    const data = await fs.promises.readFile(configJsonPath);
+    const jsonData = JSON.parse(data.toString());
+    const progress = jsonData.progress;
+    if (progress) {
+      return {
+        success: true,
+        progress,
+      };
+    }
+    return {
+      success: false,
+    };
+  } catch (err) {
+    console.error('Error getting download progress:', err);
+    return { success: false };
+  }
+};
+
 const updateDefaultDownloadPath = async (defaultDownloadPath: string) => {
   const configJsonPath = getConfigJsonPath();
   try {
@@ -47,4 +68,40 @@ const updateDefaultDownloadPath = async (defaultDownloadPath: string) => {
   }
 };
 
-export { getDefaultDownloadPath, updateDefaultDownloadPath };
+const updateDownloadProgress = async ({
+  novelName,
+  numberOfChapters,
+  currentChapter,
+  progress,
+}: {
+  novelName: string;
+  numberOfChapters: number;
+  currentChapter: number;
+  progress: number;
+}) => {
+  const configJsonPath = getConfigJsonPath();
+  try {
+    const jsonData = await getDownloadData();
+    if (!jsonData) {
+      return false;
+    }
+    jsonData.progress = {
+      novelName,
+      numberOfChapters,
+      currentChapter,
+      progress,
+    };
+    await fs.promises.writeFile(configJsonPath, JSON.stringify(jsonData));
+    return true;
+  } catch (err) {
+    console.error('Error updating download progress:', err);
+    return false;
+  }
+};
+
+export {
+  getDefaultDownloadPath,
+  updateDefaultDownloadPath,
+  getDownloadProgress,
+  updateDownloadProgress,
+};
