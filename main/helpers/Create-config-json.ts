dc325ccceda0fdb1bd62a9745a715049e9772b13
Amine derbal <aminedoran31@gmail.com>
2023-11-02 15:43:21 +0100

    Add progress to the config by default

--- main/helpers/Create-config-json.ts
+++ main/helpers/Create-config-json.ts
@@ -3,6 +3,12 @@ import fs from 'fs';
 export default (configPath: string, defaultDownloadPath: string) => {
   const configData = {
     defaultDownloadPath,
+    progress: {
+      novelName: '',
+      numberOfChapters: 0,
+      currentChapter: 0,
+      progress: 0,
+    },
   };
   if (!fs.existsSync(configPath)) {
     fs.writeFileSync(configPath, JSON.stringify(configData));
