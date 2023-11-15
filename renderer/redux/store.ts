c3be25732ced37694527af58cca36cfdce112f48
Amine derbal <aminedoran31@gmail.com>
2023-11-15 13:27:48 +0100

    Add download slice to store

--- renderer/redux/store.ts
+++ renderer/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import novelReducer from './novel/novelSlice';
 import modalReducer from './modal/modalSlice';
+import downloadReducer from './download/downloadSlice';
 
 const store = configureStore({
   reducer: {
     novel: novelReducer,
     modal: modalReducer,
+    download: downloadReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>; // Define and export RootState type
