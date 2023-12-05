d2767b35b6a726b115079bcdf83994818a4488d0
Amine derbal <aminedoran31@gmail.com>
2023-12-05 16:38:28 +0100

    Implement API to Delete download

--- renderer/pages/api/novel/download/index.ts
+++ renderer/pages/api/novel/download/index.ts
@@ -1,4 +1,5 @@
 import buildDownload from '../../../../services/buildDownload';
+import { deletePdfFile } from '../../../../services/downloadPdf';
 
 const handler = async (req: any, res: any) => {
   if (req.method === 'POST') {
@@ -15,6 +16,21 @@ const handler = async (req: any, res: any) => {
       return res
         .status(500)
         .json({ error: 'An error occurred while fetching the data' });
+    }
+  } else if (req.method === 'DELETE') {
+    const { name } = req.body;
+    try {
+      const response = await deletePdfFile(name);
+      if (!response.success) {
+        return res.status(500).json({
+          message: 'An error occurred while deleting the file',
+        });
+      }
+      return res.json({ message: 'Novel deleted successfully' });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'An error occurred while deleting the file',
+      });
     }
   }
   return res.status(400).json({ error: 'Invalid request method' });
