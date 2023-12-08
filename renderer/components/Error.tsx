9413f8ffb5829117502e8efc29541c8573336921
Amine derbal <aminedoran31@gmail.com>
2023-12-08 15:58:26 +0100

    Create error component

---
+++ renderer/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Error = () => {
+  return (
+    <div className="mt-10">
+      <h2 className="text-center text-3xl">An error occured</h2>
+      <p className="text-center text-xl">Please try again</p>
+    </div>
+  );
+};
+
+export default Error;
